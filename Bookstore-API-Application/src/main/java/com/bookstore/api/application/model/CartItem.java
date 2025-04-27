274363f1e49cafb476c12248af4068d566f9dd6c
Sandewdi Nowanya <nowanyafonseka@gmail.com>
2025-04-27 21:42:22 +0530

    Implement basic data models

--- Bookstore-API-Application/src/main/java/com/bookstore/api/application/model/CartItem.java
+++ Bookstore-API-Application/src/main/java/com/bookstore/api/application/model/CartItem.java
@@ -10,4 +10,48 @@ package com.bookstore.api.application.model;
  */
 public class CartItem {
     
+    private Long bookId;
+    private int quantity;
+    private double unitPrice;  // Storing the price at the time it was added to cart
+
+    // Default constructor
+    public CartItem() {
+    }
+
+    // Constructor with parameters
+    public CartItem(Long bookId, int quantity, double unitPrice) {
+        this.bookId = bookId;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+    }
+
+    // Getters and setters
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+    
+    // Calculate total price for this item
+    public double getTotalPrice() {
+        return unitPrice * quantity;
+    }
 }
